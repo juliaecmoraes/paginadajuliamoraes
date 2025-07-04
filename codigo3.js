2d1a2589c4349e431a036efa17ad025fa015c7b6
juliaecmoraes <juliaecdemoraes@gmail.com>
2025-07-03 21:26:21 -0300

    Add files via upload
    criando

---
+++ codigo3.js
@@ -0,0 +1,4 @@
+ function verMensagem() {
+      let elemento = document.getElementById("mensagem");
+      elemento.textContent = "Você consegue alçancar todos os seus objetivos, basta ser persistente!";
+    }
